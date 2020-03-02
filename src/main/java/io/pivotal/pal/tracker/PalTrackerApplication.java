374f82bda4cda511854f20b5f6bd20f7a447c69f
Prabhu V K <n0257952@libp45p-25768dq.mis.lmig.com>
2020-03-02 13:06:39 -0500

    chapter 7

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -4,13 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class PalTrackerApplication {
     public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
     @Bean
-    TimeEntryRepository timeEntryRepository() {
-        return new InMemoryTimeEntryRepository();
+        //    public TimeEntryRepository timeEntryRepository()
+//    {
+//        return new InMemoryTimeEntryRepository();
+//    }
+    JdbcTimeEntryRepository jdbcTimeEntryRepository(DataSource dataSource) {
+        return new JdbcTimeEntryRepository(dataSource);
     }
 }
